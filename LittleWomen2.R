fc5ed12161330cceb473d55fd4872f9bf5e7d477
Noriko Tamari <norikotamari@email.arizona.edu>
2024-02-18 16:17:21 -0700

    Initial commit

---
+++ LittleWomen2.R
@@ -0,0 +1,12 @@
+little_women <- data.frame(Name = c("Meg", "Beth", "Jo", "Amy"), 
+                           Time.Mentioned=c(683, 459, 1355, 645))
+
+# create plot object
+Littlewomen_plot <- ggplot(data=little_women,
+                           mapping =aes(x = Name, y = Time.Mentioned)) +
+  geom_bar(stat="identity") +
+  ylab("Times Mentioned")
+
+# Draw plot
+print(Littlewomen_plot)
+
